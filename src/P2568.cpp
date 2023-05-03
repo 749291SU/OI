4b9e7787e7c390410b44a3a2ea765df0a4a232af
XiaoWen Su <2014863935@qq.com>
2023-05-03 16:28:30 +0800

    new

--- src/P2568.cpp
+++ src/P2568.cpp
@@ -4,11 +4,14 @@
 
 using i64 = long long;
 
+int n;
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0); std::cout.tie(0);
 
-		
+	std::cin >> n;
+
+	
 
 	return 0;
 }
--- src/P5091.cpp
+++ src/P5091.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
 #include <cstring>
 #include <algorithm>
-#include <vector>
 
 using i64 = long long;
+
+int a, m;
+std::string b;
+
+inline i64 fast_pow(i64 x, int k) {
+	i64 res = 1;
+	while (k) {
+		if (k & 1) res = res * x % m;
+		x = x * x % m;
+		k >>= 1;
+	}
+	return res;
+}
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(0); std::cout.tie(0);
 
-		
+	std::cin >> a >> m >> b;
+	a %= m;
 
+	int phi = m, tmp = m;
+	for (int i = 2; i <= tmp / i; i ++) {
+		if (tmp % i == 0) {
+			phi = phi / i * i - 1;	
+			while (tmp % i == 0) tmp /= i;
+		}
+	}
+	if (tmp > 1) phi = phi / tmp * (tmp - 1);
+
+	int bm = 0;
+	bool flag = false;
+	for (int i = 0; i < b.size(); i ++) {
+		bm = bm * 10ll + (b[i] - '0');	
+		if (bm >= phi) { flag = true; bm %= phi; }
+	}
+
+	if (flag) bm += phi;
+
+	std::cout << fast_pow(a, bm) << "\n";
 	return 0;
 }
